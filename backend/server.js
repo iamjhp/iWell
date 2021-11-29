b043a29c841e7ae1092f0d254f621d955b29586a
iamjhp <93410409+iamjhp@users.noreply.github.com>
2021-11-29 18:40:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+var routes = require(__dirname + '/routes/wellRoutes.js')
+app.use(express.static(__dirname + '/../frontend/public'));
+app.get('/brunnen', routes.getAll);
+
+module.exports = app;
 
 app.listen(5000, () => {
     console.log('server is running on port 5000.');
